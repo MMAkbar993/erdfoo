44e059104d35e818f39fa5704c2c77c850c2cefc
M M Akbar <akbarmm428@gmail.com>
2025-12-20 01:58:02 +0500

    update

--- src/components/SponsorsSection.vue
+++ src/components/SponsorsSection.vue
@@ -1,14 +1,14 @@
 <template>
   <section class="sponsers">
     <div class="container headingContainer">
-      <div class="row d-flex justify-end" style="direction: rtl;">
+      <div class="row d-flex" style="direction: rtl;">
         <div class="box">
           <h1 style="text-align: right;">قسم الشركاء</h1>
         </div>
       </div>
     </div>
-    <div class="container">
-      <div class="row">
+    <div class="container sponsors-container">
+      <div class="row sponsors-row">
         <!-- Swiper -->
         <div class="swiper mySwiperSponser" ref="sponsorsSwiper">
           <div class="swiper-wrapper">
@@ -32,10 +32,9 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue'
 import { Swiper } from 'swiper'
-import { Navigation, Autoplay, Pagination } from 'swiper/modules'
+import { Navigation, Autoplay } from 'swiper/modules'
 import 'swiper/css'
 import 'swiper/css/navigation'
-import 'swiper/css/pagination'
 
 let swiperInstance = null
 const sponsorsSwiper = ref(null)
@@ -64,7 +63,7 @@ const sponsors = ref([
 onMounted(() => {
   if (sponsorsSwiper.value) {
     swiperInstance = new Swiper(sponsorsSwiper.value, {
-      modules: [Navigation, Autoplay, Pagination],
+      modules: [Navigation, Autoplay],
       direction: 'horizontal',
       rtl: true,
       slidesPerView: 1,
@@ -72,10 +71,6 @@ onMounted(() => {
       autoplay: {
         delay: 2500,
         disableOnInteraction: false
-      },
-      pagination: {
-        el: sponsorsSwiper.value.querySelector('.swiper-pagination'),
-        clickable: true
       },
       breakpoints: {
         240: {
@@ -112,9 +107,60 @@ onUnmounted(() => {
 </script>
 
 <style scoped>
+.sponsors-container {
+  padding: 50px 50px !important;
+  overflow: hidden;
+  max-width: 100%;
+}
+
+.sponsors-row {
+  position: relative;
+  overflow: visible;
+  width: 100%;
+  max-width: 100%;
+}
+
 .mySwiperSponser {
-  width: 95%;
+  width: 100%;
   height: 100%;
+  overflow: hidden;
+  padding: 0;
+  position: relative;
+}
+
+.mySwiperSponser :deep(.swiper-wrapper) {
+  align-items: center;
+}
+
+.mySwiperSponser :deep(.swiper-slide) {
+  flex-shrink: 0;
+  width: 98px !important;
+  height: 100px;
+}
+
+/* Ensure navigation buttons are visible and properly positioned */
+.mySwiperSponser :deep(.swiper-button-next),
+.mySwiperSponser :deep(.swiper-button-prev) {
+  position: absolute;
+  z-index: 10;
+}
+
+@media (max-width: 1024px) {
+  .sponsors-container {
+    padding: 50px 30px !important;
+  }
+}
+
+@media (max-width: 768px) {
+  .sponsors-container {
+    padding: 50px 20px !important;
+  }
+}
+
+@media (max-width: 480px) {
+  .sponsors-container {
+    padding: 50px 10px !important;
+  }
 }
 </style>
 
